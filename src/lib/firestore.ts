99940ad1ad1be2d03becfad50e3c3828ea7dfa4e
Valery Ruth Ayasta Leonardo <ValeryAyasta@gmail.com>
2025-09-18 01:18:56 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Expected first arg

--- src/lib/firestore.ts
+++ src/lib/firestore.ts
@@ -5,13 +5,16 @@ import { getFirestore, collection, doc, updateDoc, getDocs, writeBatch } from 'f
 import type { Participant } from '@/lib/types';
 import { initialParticipants } from '@/lib/participants';
 
+
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCiGDghs77Nwy5h3upjK-kzQTwR5fjO4k4",
+  authDomain: "control-asistencia-e7499.firebaseapp.com",
+  projectId: "control-asistencia-e7499",
+  databaseURL: "https://control-asistencia-e7499-default-rtdb.firebaseio.com",
+  storageBucket: "control-asistencia-e7499.firebasestorage.app",
+  messagingSenderId: "839935230948",
+  appId: "1:839935230948:web:a0d66d17a6ad8d87b8fdf1",
+  measurementId: "G-26GT07D7D6"
 };
 
 // Initialize Firebase for server-side
